c3704870fe93541c5de4f79412ab2111f0ad8391
Austin Nieset <ajnieset@gmail.com>
2023-06-21 12:28:35 -0400

    implemented basic send for sendgrid

---
+++ email_client/providers/sendgrid.py
@@ -0,0 +1,18 @@
+import sendgrid
+from sendgrid.helpers.mail import Email, To, Content, Mail
+
+from email_client.email_client import EmailClient
+
+
+class SendgridClient(EmailClient):
+    def __init__(self, api_key: str, domain: str = "example.com"):
+        self.client = sendgrid.SendGridAPIClient(api_key=api_key)
+        self.domain = domain
+
+    def send(self, to_email: str):
+        from_email = Email("test@example.com")
+        to_email = To(to_email)
+        subject = "Sending with SendGrid is Fun"
+        content = Content("text/plain", "and easy to do anywhere, even with Python")
+        mail = Mail(from_email, to_email, subject, content)
+        return self.client.client.mail.send.post(request_body=mail.get())
